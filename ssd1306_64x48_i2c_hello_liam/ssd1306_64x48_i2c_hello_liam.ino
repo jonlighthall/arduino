a6bdd990ed4f977b87fdd083b47401fad918f5f2
Jon Lighthall <jon.lighthall@gmail.com>
2021-04-18 15:16:29 -0500

    remove unused includes and logo def

--- ssd1306_64x48_i2c_hello_liam/ssd1306_64x48_i2c_hello_liam.ino
+++ ssd1306_64x48_i2c_hello_liam/ssd1306_64x48_i2c_hello_liam.ino
@@ -16,37 +16,14 @@ BSD license, check license.txt for more information
 All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
 
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 // SCL GPIO5
 // SDA GPIO4
 #define OLED_RESET 0  // GPIO0
-Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_SSD1306 display(OLED_RESET); // define display
 
 #define DELAY 200
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
 
 #if (SSD1306_LCDHEIGHT != 48)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
@@ -70,7 +47,6 @@ void setup()   {
 }
 
 void loop() {
-    // text display tests
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
