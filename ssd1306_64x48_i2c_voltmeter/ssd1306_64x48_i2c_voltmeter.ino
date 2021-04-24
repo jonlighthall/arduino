0b73bfbf649c7c1691a59a2e872dd10f833b27c9
Jon Lighthall <jon.lighthall@gmail.com>
2021-04-24 12:09:17 -0500

    add volt meter

---
+++ ssd1306_64x48_i2c_voltmeter/ssd1306_64x48_i2c_voltmeter.ino
@@ -0,0 +1,65 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 64x48 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+f
+Written by Limor Fried/Ladyada  for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <Adafruit_SSD1306.h>
+
+// SCL GPIO5
+// SDA GPIO4
+#define OLED_RESET 0  // GPIO0
+Adafruit_SSD1306 display(OLED_RESET); // define display
+
+#define DELAY 250
+
+#if (SSD1306_LCDHEIGHT != 48)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+//const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+
+//int sensorValue = 0;  // value read from the pot
+
+void setup()   {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
+  display.display();
+  delay(DELAY);
+  display.clearDisplay();
+}
+
+void loop() {
+display.clearDisplay();
+display.setCursor(0,0); 
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.println("Reading A0");
+
+//read sensor
+int sensorValue = analogRead(A0);
+char buf[64];
+sprintf(buf,"%04d\n",sensorValue);
+display.print(buf); 
+float scale = sensorValue/1023.;
+sprintf(buf,"%7.3f%%\n",scale*100.);
+display.print(buf); 
+float vmax=3.3;
+float voltage = (scale)*vmax; 
+sprintf(buf,"%5.4f V\n",voltage);
+display.print(buf); 
+display.display();
+delay(42);
+}
