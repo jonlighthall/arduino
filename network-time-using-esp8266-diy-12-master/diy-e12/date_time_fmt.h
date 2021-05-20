32ab0bddcd970d06ba40758098ce85544a7ae405
Jon Lighthall <jon.lighthall@gmail.com>
2021-05-20 15:57:29 -0500

    Add date_time_fmt header Clean up code formatting update date format add month name

---
+++ network-time-using-esp8266-diy-12-master/diy-e12/date_time_fmt.h
@@ -0,0 +1,96 @@
+/*
+ * The time structure is called tm and has teh following values:
+
+  Definition of struct tm:
+  Member  Type  Meaning Range
+  tm_sec  int seconds after the minute  0-61*
+  tm_min  int minutes after the hour  0-59
+  tm_hour int hours since midnight  0-23
+  tm_mday int day of the month  1-31
+  tm_mon  int months since January  0-11
+  tm_year int years since 1900
+  tm_wday int days since Sunday 0-6
+  tm_yday int days since January 1  0-365
+  tm_isdst  int Daylight Saving Time flag
+
+  because the values are somhow akwardly defined, I introduce a function makeHumanreadable() where all values are adjusted according normal numbering.
+  e.g. January is month 1 and not 0 And Sunday or monday is weekday 1 not 0 (according definition of MONDAYFIRST)
+
+ */
+
+char * getDOW(uint8_t tm_wday)
+{
+  switch(tm_wday)
+  {
+    case 1:
+      return "Monday";
+      break;
+    case 2:
+      return "Tuesday";
+      break;
+    case 3:
+      return "Wednesday";
+      break;
+    case 4:
+      return "Thursday";
+      break;
+    case 5:
+      return "Friday";
+      break;
+    case 6:
+      return "Saturday";
+      break;
+    case 7:
+      return "Sunday";
+      break;
+    default:
+      return "Error";
+      break;
+  }
+}
+
+char * getMo(uint8_t tm_mon)
+{
+  switch(tm_mon)
+  {
+    case 1:
+      return "Jan";
+      break;
+    case 2:
+      return "Feb";
+      break;
+    case 3:
+      return "Mar";
+      break;
+    case 4:
+      return "Apr";
+      break;
+    case 5:
+      return "May";
+      break;
+    case 6:
+      return "Jun";
+      break;
+    case 7:
+      return "Jul";
+      break;
+      case 8:
+      return "Aug";
+      break;
+    case 9:
+      return "Sep";
+      break;
+    case 10:
+      return "Oct";
+      break;
+    case 11:
+      return "Nov";
+      break;
+    case 12:
+      return "Dec";
+      break;
+    default:
+      return "Error";
+      break;
+  }
+}
--- network-time-using-esp8266-diy-12-master/diy-e12/diy-e12.ino
+++ network-time-using-esp8266-diy-12-master/diy-e12/diy-e12.ino
@@ -19,23 +19,7 @@
   - getTimeReducedTraffic(int ms) where ms is the the time between two physical NTP server calls. Betwwn these calls,
   the time structure is updated with the (inaccurate) timer. If you call NTP every few minutes you should be ok
 
-  The time structure is called tm and has teh following values:
-
-  Definition of struct tm:
-  Member  Type  Meaning Range
-  tm_sec  int seconds after the minute  0-61*
-  tm_min  int minutes after the hour  0-59
-  tm_hour int hours since midnight  0-23
-  tm_mday int day of the month  1-31
-  tm_mon  int months since January  0-11
-  tm_year int years since 1900
-  tm_wday int days since Sunday 0-6
-  tm_yday int days since January 1  0-365
-  tm_isdst  int Daylight Saving Time flag
-
-  because the values are somhow akwardly defined, I introduce a function makeHumanreadable() where all values are adjusted according normal numbering.
-  e.g. January is month 1 and not 0 And Sunday or monday is weekday 1 not 0 (according definition of MONDAYFIRST)
-
+  
   Showtime is an example on how you can use the time in your sketch
 
   The functions are inspired by work of G6EJD ( https://www.youtube.com/channel/UCgtlqH_lkMdIa4jZLItcsTg )
@@ -49,6 +33,7 @@
 #include <time.h>
 #include <U8x8lib.h>
 #include "credentials.h"
+#include "date_time_fmt.h"
 
 #define DELAY 250
 
@@ -64,8 +49,7 @@ unsigned long lastEntryTime;
 
 U8X8_SSD1306_64X48_ER_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   // EastRising 0.66" OLED breakout board, Uno: A4=SDA, A5=SCL, 5V powered
 
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
 
   u8x8.begin();
@@ -78,8 +62,7 @@ void setup()
 
   u8x8.print("Connecting to network");
   int counter = 0;
-  while (WiFi.status() != WL_CONNECTED) 
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(200);    
     if (++counter > 100) 
       ESP.restart();
@@ -92,12 +75,10 @@ void setup()
   // See https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv for Timezone codes for your region
   setenv("TZ", TZ_INFO, 1);
 
-  if (getNTPtime(10)) 
-  {  
+  if (getNTPtime(10)) {  
     // wait up to 10sec to sync
   } 
-  else 
-  {
+  else {
     u8x8.println("Time not set");
     ESP.restart();
   }
@@ -107,19 +88,15 @@ void setup()
   lastEntryTime = millis();
 }
 
-void loop() 
-{
+void loop() {
   getNTPtime(10);
   showTime(&timeinfo);
   delay(1000);
 }
 
-bool getNTPtime(int sec) 
-{
-  {
+bool getNTPtime(int sec) {
     uint32_t start = millis();
-    do
-    {
+    do {
       time(&now);
       localtime_r(&now, &timeinfo);
       delay(10);
@@ -130,12 +107,10 @@ bool getNTPtime(int sec)
     
     u8x8.print("Time Now: ");  
     u8x8.println(now); 
-  }
   return true;
 }
 
-void showTime(tm *localTime) 
-{
+void showTime(tm *localTime) {
   //display on OLED
   char time_output[30];
   
@@ -143,38 +118,9 @@ void showTime(tm *localTime)
   u8x8.home();
   sprintf(time_output, "%02d:%02d:%02d", localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
   u8x8.println(time_output);
-  sprintf(time_output, "%02d/%02d/%02d", localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year - 100);
-  u8x8.println(time_output);
   u8x8.println(getDOW(localTime->tm_wday));
-}
-
-char * getDOW(uint8_t tm_wday)
-{
-  switch(tm_wday)
-  {
-    case 1:
-      return "Monday";
-      break;
-    case 2:
-      return "Tuesday";
-      break;
-    case 3:
-      return "Wednesday";
-      break;
-    case 4:
-      return "Thursday";
-      break;
-    case 5:
-      return "Friday";
-      break;
-    case 6:
-      return "Saturday";
-      break;
-    case 7:
-      return "Sunday";
-      break;
-    default:
-      return "Error";
-      break;
-  }
+  //sprintf(time_output, "%02d/%02d/%02d", localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year - 100);
+  //sprintf(time_output, "%02d/%02d/%02d", localTime->tm_mon + 1, localTime->tm_mday, localTime->tm_year - 100);
+  sprintf(time_output, "  %s %02d", getMo(localTime->tm_mon + 1), localTime->tm_mday); 
+  u8x8.println(time_output);  
 }
