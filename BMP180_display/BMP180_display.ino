c6c77d201e8e769cde2e0f9f9e6f08e4dcb64198
Jon Lighthall <jon.lighthall@gmail.com>
2021-06-17 09:51:57 -0500

    format and add display

--- BMP180_display/BMP180_display.ino
+++ BMP180_display/BMP180_display.ino
@@ -1,18 +1,18 @@
 #include <Adafruit_BMP085.h>
 
-/*************************************************** 
+/***************************************************
   This is an example for the BMP085 Barometric Pressure & Temp Sensor
 
-  Designed specifically to work with the Adafruit BMP085 Breakout 
+  Designed specifically to work with the Adafruit BMP085 Breakout
   ----> https://www.adafruit.com/products/391
 
   These pressure and temperature sensors use I2C to communicate, 2 pins
   are required to interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
@@ -23,43 +23,58 @@
 // EOC is not used, it signifies an end of conversion
 // XCLR is a reset pin, also not used here
 
+#include <U8g2lib.h>
+
 Adafruit_BMP085 bmp;
-  
+
+U8G2_SSD1306_64X48_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // EastRising 0.66" OLED breakout board, Uno: A4=SDA, A5=SCL, 5V powered
+
 void setup() {
   Serial.begin(9600);
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
+  u8g2.begin();
+  u8g2.clearBuffer();
 }
-  
-void loop() {
-    Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-    
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
 
-    Serial.print("Pressure at sealevel (calculated) = ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
+void loop() {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_timB08_tr);
+  int texthei = u8g2.getAscent();
+  int ypos = texthei;
+  char buff[64];
+  sprintf(buff, "T = %.1f", bmp.readTemperature());
+  u8g2.drawStr(0, ypos, buff);
+
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  // Calculate altitude assuming 'standard' barometric
+  // pressure of 1013.25 millibar = 101325 Pascal
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
 
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
-    Serial.print("Real altitude = ");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.println(" meters");
-    
-    Serial.println();
-    delay(500);
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.println(" meters");
+
+  Serial.println();
+  u8g2.sendBuffer();
+  delay(500);
 }
