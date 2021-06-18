d8472b4e4732e9e94d84938196f2c3213200781a
Jon Lighthall <jon.lighthall@gmail.com>
2021-06-18 18:08:46 -0500

    add credentials file from Send_Text (working! text email sent)

---
+++ libraries/NTP_Display_Kit/credentials.h
@@ -0,0 +1,8 @@
+// add your Wi-Fi network name and password on the following lines
+// do not commit when modified
+#define WIFI_SSID "network_name"
+#define WIFI_PASSWORD "network_password"
+
+/* The log in credentials */
+#define AUTHOR_EMAIL "email_name@gmail.com"
+#define AUTHOR_PASSWORD "email_password"
