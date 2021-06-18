1df768ae8c8f145c475d4be1f4999b0385494417
Jon Lighthall <jon.lighthall@gmail.com>
2021-06-18 18:08:46 -0500

    working! text email sent

---
+++ Send_Text/credentials.h
@@ -0,0 +1,8 @@
+// add your Wi-Fi network name and password on the following lines
+// do not commit when modified
+#define WIFI_SSID "Lighthall Home"
+#define WIFI_PASSWORD "wildturkey"
+
+/* The log in credentials */
+#define AUTHOR_EMAIL "jonathan.lighthall@gmail.com"
+#define AUTHOR_PASSWORD "steward1423"
