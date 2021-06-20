fed4524bd01a28e0c2290de874346f236bf98cf8
Jon Lighthall <jon.lighthall@gmail.com>
2021-06-20 12:56:39 -0500

    add dice prints

--- random/random.ino
+++ random/random.ino
@@ -3,10 +3,41 @@ long randNumber;
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(0));
+  char buff[64];
+  sprintf(buff, "\nDice Example");
+  Serial.println(buff);
 }
 
 void loop() {
-  randNumber = random(6);
-  Serial.println(randNumber);
+  int j = random(6) + 1;
+  int k = random(6) + 1;
+  char buff[64];
+  sprintf(buff, "%d + %d = %2d", j, k, j + k);
+  Serial.print (buff);
+  if (j == k) {
+    Serial.println(" DOUBLE 1");
+    j = random(6) + 1;
+    k = random(6) + 1;
+    sprintf(buff, "%d + %d = %2d", j, k, j + k);
+    Serial.print (buff);
+    if (j == k) {
+      Serial.println(" DOUBLE 2");
+      j = random(6) + 1;
+      k = random(6) + 1;
+      sprintf(buff, "%d + %d = %2d", j, k, j + k);
+      Serial.print (buff);
+      if (j == k) {
+        Serial.println(" DOUBLE 3 - JAIL");
+        delay(2000);
+      }
+      else
+        Serial.println();
+    }
+    else
+      Serial.println();
+  }
+  else
+    Serial.println();
   delay(50);
+  Serial.println();
 }
