0dcebd3c0afc95b674f47501a905155f0142bdd6
Jon Lighthall <jon.lighthall@gmail.com>
2021-06-22 11:30:50 -0500

    fix polarity

--- button/button.ino
+++ button/button.ino
@@ -1,10 +1,11 @@
-  
 /*
- * 1 Button Shield - Simple Push
- * Press the pushbutton to switch on the LED
- *
- * 1 Button Shield pushbutton connects pin D3 to GND
- */
+  ESP8266 Button Example
+
+   1 Button Shield - Simple Push
+   Press the pushbutton to switch on the LED
+
+   1 Button Shield pushbutton connects pin D3 to GND
+*/
 
 const int buttonPin = D3;
 const int ledPin = BUILTIN_LED;
@@ -12,21 +13,29 @@ const int ledPin = BUILTIN_LED;
 int buttonState = 0;
 
 void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  char buff[64];
+  sprintf(buff, "\nButton");
+  Serial.println(buff);
+
   pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
 
   // set initial state, LED off
-  digitalWrite(ledPin, buttonState);
+  digitalWrite(ledPin, HIGH); // the LED is is active low on the ESP-01
 }
 
 void loop() {
-  // read button state, HIGH when pressed, LOW when not
+  // read button state, HIGH when not pressed, LOW when pressed
   buttonState = digitalRead(buttonPin);
-
   // if the push button pressed, switch on the LED
-  if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);  // LED on
+  if (buttonState == LOW) {
+    digitalWrite(ledPin, LOW);  // LED on
+    Serial.println("on");
   } else {
-    digitalWrite(ledPin, LOW); // LED off
+    digitalWrite(ledPin, HIGH); // LED off
+    Serial.println("off");
   }
+  delay(100);
 }
