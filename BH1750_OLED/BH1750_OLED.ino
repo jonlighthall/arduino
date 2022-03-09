5b60608b3a738c7c8d35d49a2c10779689dbfb09
Jon Lighthall <jon.lighthall@gmail.com>
2022-03-09 14:25:27 -0600

    add averaging

--- BH1750_OLED/BH1750_OLED.ino
+++ BH1750_OLED/BH1750_OLED.ino
@@ -17,8 +17,8 @@ char buff[64];
 
 void setup() {
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
-  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
   display.clearDisplay();
   display.setTextSize(1);
   display.setCursor(0, 0);
@@ -31,9 +31,11 @@ void setup() {
 }
 
 // define variables needed between loops
-float lux_last=0;
-float lux_av=0;
-float lux_av_last=0;
+float lux_last = 0;
+int N = 0;
+float lux_av_last = 0;
+const int av_int = 5;
+float values[av_int] = {0};
 
 void loop() {
   // Clear the buffer.
@@ -42,26 +44,54 @@ void loop() {
   display.setCursor(0, 0);
   display.setTextColor(WHITE);
 
-  if (light.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {    
+  if (light.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
     float lux = light.readLightLevel();
-    sprintf(buff, " L:%7.1f",lux);    
-    Serial.print(buff);
-    display.println(buff);    
-    
-    // calculate change in lux
-    float dlux = (lux - lux_last);    
-    sprintf(buff, "dL:%7.1f",dlux);    
+    N++;
+    Serial.print(N);
+    sprintf(buff, " L:%7.1f", lux);
     Serial.print(buff);
     display.println(buff);
-    
-    lux_last=lux; 
+
+    // calculate change in lux
+    float dlux = (lux - lux_last);
+    sprintf(buff, "dL:%7.1f", dlux);
+    Serial.println(buff);
+    display.println(buff);
+
+    int idx = N % av_int;
+    Serial.println(idx);
+
+    values[idx] = lux;
+
+    float lux_sum = 0;
+    int n = 0;
+    for (int i = 0; i < av_int; i ++) {
+      Serial.println(values[i]);
+      if (values[i] > 0 ) {
+        lux_sum += values[i];
+        n++;
+      }
+    }
+    float lux_av  = lux_sum / n;
+    sprintf(buff, "SL:%7.1f", lux_av);
+    Serial.println(buff);
+    display.println(buff);
+
+
+    float dlux_av =  (lux_av - lux_av_last);
+    sprintf(buff, "dS:%7.1f", dlux_av);
+    Serial.println(buff);
+    display.println(buff);
+
+    lux_last = lux;
+    lux_av_last = lux_av;
   }
   else
-    {
-      display.println("Error!");
-      display.println("BH1750 not found");
-      display.println("Light meter missing");
-    }
+  {
+    display.println("Error!");
+    display.println("BH1750 not found");
+    display.println("Light meter missing");
+  }
   display.display();
   delay(1000);
 }
