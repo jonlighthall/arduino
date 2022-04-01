ca478d16d50614034529c33bc3872f9f587d443a
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-01 11:02:52 -0500

    add display options

--- NTP_OLED_TM1367/seven-segment_text.h
+++ NTP_OLED_TM1367/seven-segment_text.h
@@ -7,7 +7,28 @@ const uint8_t SEG_DONE[] = {
 
 const uint8_t SEG_SYNC[] = {
   SEG_A | SEG_C | SEG_D | SEG_F | SEG_G ,          // S
-  SEG_B | SEG_C | SEG_D | SEG_F | SEG_G ,   // y
+  SEG_B | SEG_C | SEG_D | SEG_F | SEG_G ,          // y
   SEG_C | SEG_E | SEG_G,                           // n
-  SEG_A | SEG_D | SEG_E | SEG_F             // C
+  SEG_A | SEG_D | SEG_E | SEG_F                    // C
+};
+
+const uint8_t SEG_HEllO[] = {
+  SEG_B | SEG_C | SEG_E | SEG_F | SEG_G ,          // H
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,           // E
+  SEG_B | SEG_C | SEG_E | SEG_F,                   // l l
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F    // O
+};
+
+const uint8_t SEG_hEllo[] = {
+  SEG_C | SEG_E | SEG_F | SEG_G ,                  // h
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,           // E
+  SEG_B | SEG_C | SEG_E | SEG_F,                   // l l
+  SEG_C | SEG_D | SEG_E | SEG_G                    // o
+};
+
+const uint8_t SEG_CONN[] = {
+  SEG_A | SEG_D | SEG_E | SEG_F,                   // C
+  SEG_C | SEG_D | SEG_E | SEG_G,                   // o
+  SEG_C | SEG_E | SEG_G,                           // n
+  SEG_C | SEG_E | SEG_G                            // n
 };
