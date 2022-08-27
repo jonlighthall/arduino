a6f4a8dcf6d4a03941478f35eefb0f4adb54b18f
Jon Lighthall <jon.lighthall@gmail.com>
2022-08-27 15:40:26 -0500

    expand seven seg definitions from DS18B20_arb_TM1637 original commit: fix TM1637 names (rename parent dir) add degree symbols add A,b,C,F add BAd,SCAn

--- libraries/NTP_Display_Kit/seven-segment_text.h
+++ libraries/NTP_Display_Kit/seven-segment_text.h
@@ -32,3 +32,43 @@ const uint8_t SEG_CONN[] = {
   SEG_C | SEG_E | SEG_G,                           // n
   SEG_C | SEG_E | SEG_G                            // n
 };
+
+const uint8_t SEG_degC[] = {
+  SEG_A | SEG_B | SEG_F | SEG_G,                   // Degree Symbol
+  SEG_A | SEG_D | SEG_E | SEG_F                    // C
+};
+
+const uint8_t SEG_degF[] = {
+  SEG_A | SEG_B | SEG_F | SEG_G,                   // Degree Symbol
+  SEG_A | SEG_E | SEG_F | SEG_G                    // F
+};
+
+const uint8_t SEG_letF[] = {
+  SEG_A | SEG_E | SEG_F | SEG_G                    // F
+};
+
+const uint8_t SEG_letA[] = {
+  SEG_A | SEG_B | SEG_F | SEG_G | SEG_E | SEG_C
+};
+
+const uint8_t SEG_letb[] = {
+  SEG_F | SEG_E | SEG_G | SEG_D | SEG_C
+};
+
+const uint8_t SEG_letC[] = {
+  SEG_A | SEG_F | SEG_E | SEG_D 
+};
+
+const uint8_t SEG_bad[] = {
+  0x00,
+  SEG_F | SEG_E | SEG_G | SEG_D | SEG_C,           // B
+  SEG_A | SEG_B | SEG_F | SEG_G | SEG_E | SEG_C,   // a
+  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G            // d
+};
+
+const uint8_t SEG_scan[] = {
+  SEG_A | SEG_C | SEG_D | SEG_F | SEG_G ,          // S
+  SEG_A | SEG_D | SEG_E | SEG_F,                   // C
+  SEG_A | SEG_B | SEG_F | SEG_G | SEG_E | SEG_C,   // a    
+  SEG_C | SEG_E | SEG_G                            // n
+};
