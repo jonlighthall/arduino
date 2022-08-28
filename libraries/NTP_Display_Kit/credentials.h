de9453edd4efc06cc796647a04f846199426719c
Jon Lighthall <jon.lighthall@gmail.com>
2022-08-28 10:11:15 -0500

    add update index instructions (strip info from credentials)

--- libraries/NTP_Display_Kit/credentials.h
+++ libraries/NTP_Display_Kit/credentials.h
@@ -1,8 +1,18 @@
+/* 
+ Use the following command to ignore changes to this files, both local
+ and upstream
+
+    git update-index --skip-worktree credentials.h
+
+ Use the following command to track changes to the fiel
+
+    git update-index --no-skip-worktree credentials.h 
+*/
+
 // add your Wi-Fi network name and password on the following lines
-// do not commit when modified
 #define WIFI_SSID "network_name"
 #define WIFI_PASSWORD "network_password"
 
-/* The log in credentials */
+// add your email username and password on the following lines
 #define AUTHOR_EMAIL "email_name@gmail.com"
 #define AUTHOR_PASSWORD "email_password"
