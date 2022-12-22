69a028c88b6ce7695c68b53ba7ba22187aa85b66
Jon Lighthall <jon.lighthall@gmail.com>
2022-12-21 19:28:00 -0600

    add wifi utilities from wifi test sleep; add serial blink

--- NTP_Client/NTP_Client.ino
+++ NTP_Client/NTP_Client.ino
@@ -3,9 +3,24 @@
 
 NTPClient timeClient(ntpUDP);
 
+#define BLINK_DELAY 50 // delay in milliseconds
+
 void setup() {
+  // initialize on-board LED
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+
   Serial.begin(9600);
-  wifi_start();
+  while (!Serial) {
+    digitalWrite(LED_BUILTIN, LOW); // on
+    delay(BLINK_DELAY);
+    digitalWrite(LED_BUILTIN, HIGH); // off
+    delay(BLINK_DELAY);
+  }
+  delay(128);
+  Serial.println("NTP Client");
+
+  wifi_con();
   timeClient.begin();
 }
 
--- NTP_Client/wifi_settings.h
+++ NTP_Client/wifi_settings.h
@@ -6,14 +6,71 @@
 const char* ssid = mySSID;          //from credentials.h file
 const char* pass = myPASSWORD;      //from credentials.h file
 
+#define PRINT_DELAY 250 // print delay in milliseconds
+
 WiFiUDP ntpUDP;
 
-void wifi_start();
+void wifi_mac() {
+  Serial.print("       MAC address : ");
+  Serial.println(WiFi.macAddress());
+}
 
-void wifi_start() {
-  WiFi.begin(ssid, pass);
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
+int wifi_stat() {
+  // print status
+  int i;
+  Serial.print("      Wi-Fi status : ");
+  i = WiFi.status();
+  Serial.println(i);
+  if (i == 3) {
+    Serial.print("gateway IP address : ");
+    Serial.println(WiFi.gatewayIP());
+    Serial.print("  local IP address : ");
+    Serial.println(WiFi.localIP());
+    Serial.print("   signal strength : ");
+    Serial.println(WiFi.RSSI());
   }
+  return i;
+}
+
+void wifi_con() {
+  Serial.println();  
+  wifi_stat();
+  Serial.print(" Exit modem sleep mode...");
+  WiFi.forceSleepWake(); // Wifi on
+  Serial.println("done");
+  wifi_stat();
+  WiFi.mode(WIFI_STA); // station mode
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.print("...");
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(PRINT_DELAY);
+    Serial.print(".");
+  }
+  Serial.print("connected\n");
+  wifi_stat();
+  //  Serial.println("set sleep type none...");
+  //  wifi_set_sleep_type(NONE_SLEEP_T);
+  //  wifi_fpm_close(); //disable force sleep function
+}
+
+void wifi_discon() {
+  wifi_stat();
+  Serial.print("Disconnecting from ");
+  Serial.print(WiFi.SSID());
+  Serial.print("...");
+  WiFi.disconnect();
+  while (WiFi.status() == WL_CONNECTED) {
+    delay(PRINT_DELAY);
+    Serial.print(".");
+  }
+  Serial.print("disconnected\n");
+  wifi_stat();
+  Serial.print("Enter modem sleep mode...");
+  WiFi.mode( WIFI_OFF );
+  WiFi.forceSleepBegin();
+  delay(1); //the modem won't go to sleep unless you do a delay
+  Serial.println("done");
+  wifi_stat();
 }
