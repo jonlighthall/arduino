343bc1b1f029deacb3276fac6b09188c83bb7238
Jon Lighthall <jon.lighthall@gmail.com>
2022-12-21 18:11:44 -0600

    add wifi settings header

--- NTP_Client/NTP_Client.ino
+++ NTP_Client/NTP_Client.ino
@@ -1,33 +1,16 @@
-
 #include <NTPClient.h>
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#include "wifi_settings.h"
 
-// Wi-Fi settings:
-#include "credentials.h"
-const char* ssid = mySSID;          //from credentials.h file
-const char* pass = myPASSWORD;      //from credentials.h file
-
-WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
-void setup(){
+void setup() {
   Serial.begin(9600);
-
-  WiFi.begin(ssid, pass);
-
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
-  }
-
+  wifi_start();
   timeClient.begin();
 }
 
 void loop() {
   timeClient.update();
-
   Serial.println(timeClient.getFormattedTime());
-
   delay(1000);
 }
