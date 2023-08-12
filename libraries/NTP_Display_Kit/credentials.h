34f20b5cfc7d4b77a25cadd1b26cc30fdae2c62b
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-12 14:15:11 -0500

    remove gmail from default author email

--- libraries/NTP_Display_Kit/credentials.h
+++ libraries/NTP_Display_Kit/credentials.h
@@ -5,11 +5,12 @@
    Use the following command to ignore changes to this files, both local
    and upstream
 
-   git update-index --skip-worktree credentials.h
+      git update-index --skip-worktree credentials.h
 
    Use the following command to track changes to the file
 
-   git update-index --no-skip-worktree credentials.h
+      git update-index --no-skip-worktree credentials.h
+
 */
 
 // add your Wi-Fi network name and password on the following lines
@@ -17,5 +18,5 @@
 #define WIFI_PASSWORD "network_password"
 
 // add your email username and password on the following lines
-#define AUTHOR_EMAIL "email_name@gmail.com"
+#define AUTHOR_EMAIL "email_user.name@domain.tld"
 #define AUTHOR_PASSWORD "email_password"
