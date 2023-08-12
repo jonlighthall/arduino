b4c292210ed2da466ca20f24a1eee068a4695c4a
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-12 14:33:05 -0500

    remove duplicate credentials (move to library)

--- NTP_OLED-diy-12/credentials.h
+++
@@ -1,8 +0,0 @@
-// add your Wi-Fi network name and password on the following lines
-// do not commit when modified
-char mySSID[] = "network_name";
-char myPASSWORD[] = "network_password";
-// to un-track:
-// git update-index --assume-unchanged credentials.h
-// to track:
-// git update-index --no-assume-unchanged credentials.h
