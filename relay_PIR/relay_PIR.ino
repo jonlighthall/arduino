d9d78a3af6c9e1aa6a384aab361e329a462213a2
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-16 15:53:56 -0500

    remove built in led feedback, add delay

--- relay_PIR/relay_PIR.ino
+++ relay_PIR/relay_PIR.ino
@@ -5,18 +5,15 @@ int PIRState = 0;
 void setup() {
   pinMode(relayPin, OUTPUT);
   pinMode(PIR, INPUT);
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, HIGH);
   digitalWrite(relayPin, LOW);
 }
 
 void loop() {
   PIRState = digitalRead(PIR);
   if (PIRState == HIGH) {
-    digitalWrite(BUILTIN_LED, LOW);  // LED on
-    digitalWrite(relayPin, HIGH); // turn on relay with voltage HIGH
+    digitalWrite(relayPin, HIGH); // turn on relay
+    delay(1000*30);
   } else {
-    digitalWrite(BUILTIN_LED, HIGH); // LED off
-    digitalWrite(relayPin, LOW);  // turn off relay with voltage LOW
+    digitalWrite(relayPin, LOW); // turn off relay
   }
 }
