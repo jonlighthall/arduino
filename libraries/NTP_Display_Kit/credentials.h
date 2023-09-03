4122e6efda2ff5b3a8ce5dc7f83237a585b48628
Jon Lighthall <jon.lighthall@gmail.com>
2023-09-02 22:55:11 -0500

    cred: expand comments, add receip email

--- libraries/NTP_Display_Kit/credentials.h
+++ libraries/NTP_Display_Kit/credentials.h
@@ -1,22 +1,29 @@
 /*
-   NB: do not commit changes to this file which include sensitive
-   information!
+  NB: do not commit changes to this file which include sensitive information!
+  
+  Use the following command to ignore changes to this files, both local and upstream
+  
+  >   git update-index --skip-worktree credentials.h
 
-   Use the following command to ignore changes to this files, both local
-   and upstream
+  Use the following command to track changes to the file
 
-      git update-index --skip-worktree credentials.h
-
-   Use the following command to track changes to the file
-
-      git update-index --no-skip-worktree credentials.h
-
+  >   git update-index --no-skip-worktree credentials.h
 */
 
 // add your Wi-Fi network name and password on the following lines
 #define WIFI_SSID "network_name"
 #define WIFI_PASSWORD "network_password"
 
-// add your email username and password on the following lines
+/*
+  Add your (the sender's) email username and password on the following lines. Since since May 30,
+  2022, Google does not allow the use of account passwords by third-party apps. Instead, Google
+  app passwords are unique 16-digit passcodes.
+   
+  e.g.   AUTHOR_PASSWORD "1234567890abcdef"
+*/
+
 #define AUTHOR_EMAIL "email_user.name@domain.tld"
 #define AUTHOR_PASSWORD "email_password"
+
+/* Recipient email address */
+#define RECIPIENT_EMAIL "email_user.name@domain.tld"
