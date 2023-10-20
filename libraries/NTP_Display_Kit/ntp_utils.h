6b9fe775ede88cb15a8468ad423bad0dc91f7061
Jon Lighthall <jon.lighthall@gmail.com>
2023-10-20 17:51:08 -0500

    add missing bracket and indent

--- libraries/NTP_Display_Kit/ntp_utils.h
+++ libraries/NTP_Display_Kit/ntp_utils.h
@@ -57,125 +57,123 @@ void parseNTP_header (byte words[]) {
   uint32_t  rootDispersion = words[3];
   uint32_t  referenceIdentifier = words[4];
 
-      Serial.print("header: ");
-      print_uint32(words[0]);
-      Serial.println();
-      // define variables
-      uint32_t  LI = getBits32(words[0], 0, 2);
-      Serial.print("\nLI: ");
-      print_uint8(LI);
-      Serial.println("should be dec 0-3 or bin 00-11");
-      uint32_t  VN = getBits32(words[0], 2, 3);
-      Serial.print("\nVN: ");
-      print_uint8(VN);
-      Serial.println("should be dec 4 or bin 100");
-      uint32_t  Mode = getBits32(words[0], 5, 3);
-      Serial.print("\nMode: ");
-      print_uint8(Mode);
+  Serial.print("header: ");
+  print_uint32(words[0]);
+  Serial.println();
+  // define variables
+  uint32_t  LI = getBits32(words[0], 0, 2);
+  Serial.print("\nLI: ");
+  print_uint8(LI);
+  Serial.println("should be dec 0-3 or bin 00-11");
+  uint32_t  VN = getBits32(words[0], 2, 3);
+  Serial.print("\nVN: ");
+  print_uint8(VN);
+  Serial.println("should be dec 4 or bin 100");
+  uint32_t  Mode = getBits32(words[0], 5, 3);
+  Serial.print("\nMode: ");
+  print_uint8(Mode);
 
-      uint32_t  Stratum = getBits32(words[0], 8, 8);
-      Serial.print("\nStra: ");
-      print_uint8(Stratum);
-      Serial.println("should be dec 0-16, hex 0-F");
+  uint32_t  Stratum = getBits32(words[0], 8, 8);
+  Serial.print("\nStra: ");
+  print_uint8(Stratum);
+  Serial.println("should be dec 0-16, hex 0-F");
 
-      uint32_t  Poll = getBits32(words[0], 16, 8);
-      Serial.print("\nPoll: ");
-      print_uint8(Poll);
-      Serial.print("int: ");
-      int8_t pinterval = int8_t(Poll);
-      Serial.print(pinterval);
-      Serial.print(" seconds\n");
-      Serial.println("8-bit signed int");
+  uint32_t  Poll = getBits32(words[0], 16, 8);
+  Serial.print("\nPoll: ");
+  print_uint8(Poll);
+  Serial.print("int: ");
+  int8_t pinterval = int8_t(Poll);
+  Serial.print(pinterval);
+  Serial.print(" seconds\n");
+  Serial.println("8-bit signed int");
 
-      uint32_t  Precision = getBits32(words[0], 24, 8);
-      Serial.print("\nPrec: ");
-      print_uint8(Precision);
-      Serial.print("int: ");
-      int8_t ppower = int8_t(Precision);
-      Serial.print(ppower);
-      Serial.print(", seconds ");
-      double sprec = pow(2, ppower);
+  uint32_t  Precision = getBits32(words[0], 24, 8);
+  Serial.print("\nPrec: ");
+  print_uint8(Precision);
+  Serial.print("int: ");
+  int8_t ppower = int8_t(Precision);
+  Serial.print(ppower);
+  Serial.print(", seconds ");
+  double sprec = pow(2, ppower);
 
-      Serial.print(sprec);
-      sprintf(buff, "precision: %.30f seconds\n", sprec);
-      Serial.print(buff);
-      Serial.println("\n8-bit signed int");     
+  Serial.print(sprec);
+  sprintf(buff, "precision: %.30f seconds\n", sprec);
+  Serial.print(buff);
+  Serial.println("\n8-bit signed int");     
 
-      sprintf(buff, "%010u Root Delay\n", rootDelay);
-      Serial.print(buff);
-      sprintf(buff, "%010u Root Dispersion\n", rootDispersion);
-      Serial.print(buff);
-      sprintf(buff, "%010u Reference Identifier\n", referenceIdentifier);
-      Serial.print(buff);
+  sprintf(buff, "%010u Root Delay\n", rootDelay);
+  Serial.print(buff);
+  sprintf(buff, "%010u Root Dispersion\n", rootDispersion);
+  Serial.print(buff);
+  sprintf(buff, "%010u Reference Identifier\n", referenceIdentifier);
+  Serial.print(buff);
 
-      if (Stratum == 1) {
-        print_binary(referenceIdentifier, 32);
-        Serial.println();
-        uint8_t a = getBits32(referenceIdentifier, 0, 8) ;
-        uint8_t b = getBits32(referenceIdentifier, 8, 8) ;
-        uint8_t c = getBits32(referenceIdentifier, 16, 8) ;
-        uint8_t d = getBits32(referenceIdentifier, 24, 8) ;
+  if (Stratum == 1) {
+    print_binary(referenceIdentifier, 32);
+    Serial.println();
+    uint8_t a = getBits32(referenceIdentifier, 0, 8) ;
+    uint8_t b = getBits32(referenceIdentifier, 8, 8) ;
+    uint8_t c = getBits32(referenceIdentifier, 16, 8) ;
+    uint8_t d = getBits32(referenceIdentifier, 24, 8) ;
 
-        sprintf(buff, "here\n");
-        Serial.print(buff);
+    sprintf(buff, "here\n");
+    Serial.print(buff);
 
-        sprintf(buff, "server id: %c%c%c%c\n", a, b, c, d);
-        Serial.print(buff);
+    sprintf(buff, "server id: %c%c%c%c\n", a, b, c, d);
+    Serial.print(buff);
 
-        // sprintf(buff, "server id: %c\n", Stratum);
-        // Serial.print(buff);      }
-
-
+    // sprintf(buff, "server id: %c\n", Stratum);
+    // Serial.print(buff);      }
 
   
+  }
 }
 
 void parseNTP_time (byte words[]) {
   char buff[64];  
   // print raw NTP time      
-      Serial.println("\nraw 64-bit timestamps");
-      for (int i = 0; i < 4; i++) {
-        sprintf(buff, "i = %1d %010u %010u %s\n", i + 1, words[4 + i * 2], words[5 + i * 2], NTP_names[i]);
-        Serial.print(buff);
-      }
+  Serial.println("\nraw 64-bit timestamps");
+  for (int i = 0; i < 4; i++) {
+    sprintf(buff, "i = %1d %010u %010u %s\n", i + 1, words[4 + i * 2], words[5 + i * 2], NTP_names[i]);
+    Serial.print(buff);
+  }
 
-      // print raw NTP time
-      uint32_t secsSince1900[4];
-      Serial.println("\nraw 32-bit timestamps (seconds)");
-      for (int i = 0; i < 4; i++) {
-        secsSince1900[i] = words[4 + i * 2];
-        sprintf(buff, "i = %1d %010u %s\n", i + 1, secsSince1900[i], NTP_names[i]);
-        Serial.print(buff);
-      }     
+  // print raw NTP time
+  uint32_t secsSince1900[4];
+  Serial.println("\nraw 32-bit timestamps (seconds)");
+  for (int i = 0; i < 4; i++) {
+    secsSince1900[i] = words[4 + i * 2];
+    sprintf(buff, "i = %1d %010u %s\n", i + 1, secsSince1900[i], NTP_names[i]);
+    Serial.print(buff);
+  }     
 
-      // convert to unix time
-      uint32_t secsSince1970[4];
-      for (int i = 0; i < 4; i++) {
-        secsSince1970[i] = secsSince1900[i] - NTP_UNIX_OFFSET_SECONDS;
-      }
+  // convert to unix time
+  uint32_t secsSince1970[4];
+  for (int i = 0; i < 4; i++) {
+    secsSince1970[i] = secsSince1900[i] - NTP_UNIX_OFFSET_SECONDS;
+  }
 
-      // print unix time
-      Serial.println("\n32-bit unix timestamps");
-      for (int i = 0; i < 4; i++) {
-        sprintf(buff, "i = %2d %010u %s\n", i, secsSince1970[i], NTP_names[i]);
-        Serial.print(buff);
-      }
+  // print unix time
+  Serial.println("\n32-bit unix timestamps");
+  for (int i = 0; i < 4; i++) {
+    sprintf(buff, "i = %2d %010u %s\n", i, secsSince1970[i], NTP_names[i]);
+    Serial.print(buff);
+  }
 
-      // convert to local time
-      uint32_t localTime[4];
-      for (int i = 0; i < 4; i++) {
-        localTime[i] = secsSince1970[i] + SetTimeZone * SECS_PER_HOUR;
-      }
+  // convert to local time
+  uint32_t localTime[4];
+  for (int i = 0; i < 4; i++) {
+    localTime[i] = secsSince1970[i] + SetTimeZone * SECS_PER_HOUR;
+  }
 
-      // print unix time
-      Serial.println("\nlocal 32-bit timestamps");
-      for (int i = 0; i < 4; i++) {
-        sprintf(buff, "i = %2d %010u %s\n", i, localTime[i], NTP_names[i]);
-        Serial.print(buff);
-      }
-      NTPlocalTime=localTime[3];
+  // print unix time
+  Serial.println("\nlocal 32-bit timestamps");
+  for (int i = 0; i < 4; i++) {
+    sprintf(buff, "i = %2d %010u %s\n", i, localTime[i], NTP_names[i]);
+    Serial.print(buff);
+  }
+  NTPlocalTime=localTime[3];
 }
-
 
 void parseNTP_fraction (byte words[]) {
   char buff[64];
